4c32134fa861bbba19b24de4afffe92681a125c0
Helen Du <helendu.614@gmail.com>
2018-03-16 15:43:45 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,6 +22,13 @@ def self.new_by_name(title)
   song
 end
 
+def self.create_by_name(name)
+  song = self.new
+  song.name = name
+  song.save
+  song
+end
+
 def self.find_by_name(title)
   self.all.find{|song| song.name == title}
 end
