d9bdad2fba565dabbcac605138b2b5e217429b05
Helen Du <helendu.614@gmail.com>
2018-03-16 15:38:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,27 +11,27 @@ class Song
   end
 
 def self.create
-  song = Song.new
-  song.save
+  song = self.new
+  self.all << song
   song
 end
 
-def self.new_by_name(name)
-  song = self.new_by_name
-  song.name = name
+def self.new_by_name(title)
+  song = self.new
+  song.name = title
   song
 end
 
-def self.find_by_name(name)
-  self.all.find{|song| song.name == name}
+def self.find_by_name(title)
+  self.all.find{|song| song.name == title}
 end
 
-def self.find_or_create_by_name(name)
-  self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+def self.find_or_create_by_name(title)
+  !self.find_by_name(title) ? self.find_by_name(title) : self.create_by_name(title)
 end
 
 def self.alphabetical
-  self.all.sort_by{|song| song.name}
+  self.all.sort_by {|song| song.name}
 end
 
 def self.new_from_filename(file)
